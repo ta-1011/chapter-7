1fd29b7d5e49a4accc471fcaa20482943f40ce2f
kuro <kkrmlove1011@gmail.com>
2026-04-17 16:39:58 +0900

    💄 UIパーツの作成

---
+++ src/components/ui/ErrorMessage.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  message: string;
+};
+
+const ErrorMessage = (props: Props) => {
+  if (props.message) {
+    return (
+      <>
+        <p>{props.message}</p>
+      </>
+    );
+  }
+};
+
+export default ErrorMessage;
---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,24 @@
+type Props = {
+  type: string;
+  value: string;
+  id?: string;
+  onChange: (value: string) => void;
+};
+
+const Input = (props: Props) => {
+  return (
+    <>
+      <input
+        type={props.type}
+        value={props.value}
+        id={props.id}
+        onChange={(e) => {
+          props.onChange(e.target.value);
+        }}
+        className="p-4 border border-gray-300 w-full"
+      />
+    </>
+  );
+};
+
+export default Input;
