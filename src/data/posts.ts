900284dc581946d8d3851452f2eb711c1313fee5
kuro <kkrmlove1011@gmail.com>
2026-04-21 14:11:28 +0900

    🚚 .tsに変更、Post[]型の付与

---
+++ src/data/posts.ts
@@ -0,0 +1,41 @@
+type Post = {
+  id: number;
+  title: string;
+  thumbnailUrl: string;
+  createdAt: string;
+  categories?: string[];
+  content: string;
+};
+
+export const posts: Post[] = [
+  {
+    id: 1,
+    title: "aaa",
+    thumbnailUrl: "https://placehold.jp/800x400.png",
+    createdAt: "2023-09-11T09:00:00.000Z",
+    categories: ["React", "TypeScript"],
+    content: `
+    本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
+  },
+  {
+    id: 2,
+    title: "記事タイトル２",
+    thumbnailUrl: "https://placehold.jp/800x400.png",
+    createdAt: "2023-09-10T09:00:00.000Z",
+    categories: ["HTML", "CSS"],
+    content: `
+    本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
+  },
+  {
+    id: 3,
+    title: "記事タイトル３",
+    thumbnailUrl: "https://placehold.jp/800x400.png",
+    createdAt: "2023-09-09T09:00:00.000Z",
+    categories: ["JavaScript"],
+    content: `
+    本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
+  },
+];
+
+//  今回使用するdataは、以下から使用する
+// https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts
